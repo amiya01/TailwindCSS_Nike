7e716028f99ef64dd488b163c8816e6e370e08a4
Amiya Ranjan Barik <amiyaranjanbrk@gmail.com>
2024-10-13 17:30:06 +0530

    Created Nav section and a small part of Hero section

---
+++ src/components/Nav.jsx
@@ -0,0 +1,32 @@
+import { headerLogo } from "../assets/images";
+import { hamburger } from "../assets/icons";
+import { navLinks } from "../constants";
+
+const Nav = () => {
+  return (
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
+          <img src={headerLogo} alt="Logo img" width={130} height={29} />
+        </a>
+        <ul className="flex flex-1 justify-center items-center gap-16 max-lg:hidden">
+          {navLinks.map((link) => (
+            <li key={link.label}>
+              <a
+                href={link.href}
+                className="font-montserrat leading-normal text-lg text-slate-gray"
+              >
+                {link.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className="lg:hidden">
+          <img src={hamburger} alt="hamburger icon" width={25} height={25} />
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Nav;
